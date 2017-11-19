4605f2dab382ae89878d0413e26a1a36ce024348
Ow Hong Yu <ohy99@hotmail.com>
2017-11-19 15:19:02 +0800

    first

---
+++ MGD/app/src/main/java/com/mgd/mgd/MainMenu.java
@@ -0,0 +1,13 @@
+package com.mgd.mgd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
